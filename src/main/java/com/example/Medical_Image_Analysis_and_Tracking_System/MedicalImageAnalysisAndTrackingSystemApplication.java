4a5d5ba4cc946f1b5448354ab32c679772926dfb
sevvalozgerr <ozger_sevval@outlook.com>
2024-10-15 01:05:17 +0300

    initial

---
+++ src/main/java/com/example/Medical_Image_Analysis_and_Tracking_System/MedicalImageAnalysisAndTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Medical_Image_Analysis_and_Tracking_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalImageAnalysisAndTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalImageAnalysisAndTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Medical_Image_Analysis_and_Tracking_System/MedicalImageAnalysisAndTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Medical_Image_Analysis_and_Tracking_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalImageAnalysisAndTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
